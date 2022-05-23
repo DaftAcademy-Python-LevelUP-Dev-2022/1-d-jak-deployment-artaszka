6f61449fd39842d67d812600780d6cdf8af7e225
Artur <31184382+artaszka@users.noreply.github.com>
2022-05-23 21:00:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,2 @@
 class HerokuApp:
-    app_url = "https://share.streamlit.io/artaszka/d_jak_deploy_2022/main/main.py/"  # Fill your heroku app url here
+    app_url = "https://arturp.herokuapp.com/"  # Fill your heroku app url here
